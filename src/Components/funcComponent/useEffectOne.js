a1ff67c687ca772bccb8783333097754eb8ae272
carmelraj <carmelraj601@gmail.com>
2023-01-25 22:56:38 +0530

    1 file modified

--- src/Components/funcComponent/useEffectOne.js
+++ src/Components/funcComponent/useEffectOne.js
@@ -1,9 +1,9 @@
 import React,{useState} from 'react';
 const UseEffectOne = () =>{
+ const [count, setCount] = useState(0)
  
- 
-  return(<><h2>Hooks Counter Two</h2>
-    
+  return(<><h2>Use Effect One</h2>
+    <button>click {count} times</button>
   </>);
 }
 export default UseEffectOne
