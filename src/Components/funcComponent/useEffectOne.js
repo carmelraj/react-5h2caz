8b147e51b64c305a0a5e88337d84c70122d6eb91
carmelraj <carmelraj601@gmail.com>
2023-02-04 21:15:16 +0530

    1 file modified

--- src/Components/funcComponent/useEffectOne.js
+++ src/Components/funcComponent/useEffectOne.js
@@ -1,9 +1,11 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 const UseEffectOne = () =>{
  const [count, setCount] = useState(0)
- 
+ useEffect(()=>{
+   document.title=`Your count is ${count}`
+ })
   return(<><h2>Use Effect One</h2>
-    <button>click {count} times</button>
+    <button onClick={()=>setCount(count+1)}>click {count} times</button>
   </>);
 }
 export default UseEffectOne
