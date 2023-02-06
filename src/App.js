0d01b5dbcb1ffe990ecee8c278dc924e5d6e1b9b
carmelraj <carmelraj601@gmail.com>
2023-02-06 22:12:05 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./style.css";
 // import GetApi from './Components/API/getAPI'
 import UseEffectOne from './Components/funcComponent/useEffectOne';
+import MouseHooks from './Components/funcComponent/mouseHooks';
 export default function App() {
   return (
     <div>
   {/* <GetApi /> */}
-  <UseEffectOne />
+  {/* <UseEffectOne /> */}
+  <MouseHooks />
   </div>
   );
 }
---
+++ src/Components/funcComponent/mouseHooks.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+const MouseHooks = () => {
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
+
+  const logMouseEvent = (e) => {
+    console.log('Mouse event');
+    setX(e.clientX);
+    setY(e.clientY);
+  };
+  useEffect(() => {
+    console.log('use effect');
+    window.addEventListener('mouseover', logMouseEvent);
+  });
+  return (
+    <>
+      <h2>Mouse Hooks</h2>
+      {x} and {y}
+    </>
+  );
+};
+export default MouseHooks;
