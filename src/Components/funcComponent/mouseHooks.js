f5565672a0cd089f2c7c762ba1a5534f5833951d
carmelraj <carmelraj601@gmail.com>
2023-02-06 22:14:57 +0530

    1 file modified

--- src/Components/funcComponent/mouseHooks.js
+++ src/Components/funcComponent/mouseHooks.js
@@ -4,14 +4,14 @@ const MouseHooks = () => {
   const [y, setY] = useState(0)
 
   const logMouseEvent = (e) => {
-    console.log('Mouse event');
-    setX(e.clientX);
-    setY(e.clientY);
+    console.log('Mouse event')
+    setX(e.clientX)
+    setY(e.clientY)
   };
   useEffect(() => {
     console.log('use effect');
     window.addEventListener('mouseover', logMouseEvent);
-  },[]);
+  },[])
   return (
     <>
       <h2>Mouse Hooks</h2>
